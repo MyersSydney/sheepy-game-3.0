b8912d58cf4c841f2064efd566df1aabd4eef77b
Sydney Myers <myerssm02@uww.edu>
2019-12-06 18:32:05 -0600

    :sunflower:

---
+++ Sheepy Game 3.0/Assets/playerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5F;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
